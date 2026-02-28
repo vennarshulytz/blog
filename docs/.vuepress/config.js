dab57deaacb6c8603f8397130d9b7136b659a665
vennarshulytz <3530163057@qq.com>
2026-02-28 15:57:03 +0800

    chore(config): 更新基础路径设置

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,6 +3,7 @@ import { defineUserConfig } from 'vuepress'
 import { viteBundler } from '@vuepress/bundler-vite'
 
 export default defineUserConfig({
+  base: '/blog/',
   lang: 'en-US',
 
   title: 'VuePress',
